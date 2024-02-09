fa3ca62cfcd6f9595233a2b58c906d3c12ba2d8b
Mahajabin Tokon <tokonislam1122@gmail.com>
2024-02-09 16:24:15 -0700

    update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Getting Started with Create React App
 
-9:43
+15:53
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
---
+++ src/components/accordian/data.js
@@ -0,0 +1,25 @@
+const data = [
+    {
+        id: "1",
+        question: "What are accodian components?",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto sequi voluptas soluta error dolore ex eum et quam, nobis, laborum sit odit, dolores autem quasi? Temporibus officia blanditiis qui soluta!"
+    },
+    {
+        id: "2",
+        question: "What are they used for?",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto sequi voluptas soluta error dolore ex eum et quam, nobis, laborum sit odit, dolores autem quasi? Temporibus officia blanditiis qui soluta!"
+    },
+    {
+        id: "3",
+        question: "Accordian is a musical instrument?",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto sequi voluptas soluta error dolore ex eum et quam, nobis, laborum sit odit, dolores autem quasi? Temporibus officia blanditiis qui soluta!"
+    },
+    {
+        id: "4",
+        question: "Can I create an accordian with different framework?",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto sequi voluptas soluta error dolore ex eum et quam, nobis, laborum sit odit, dolores autem quasi? Temporibus officia blanditiis qui soluta!"
+    },
+];
+
+
+export default data;
